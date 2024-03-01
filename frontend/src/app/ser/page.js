0e6721d5712d304ced935c62587423c9354c75ef
Billy_54 <witek.trabka@gmail.com>
2024-03-01 13:56:53 +0100

    done navbar and search bar

---
+++ frontend/src/app/ser/page.js
@@ -0,0 +1,58 @@
+"use client"
+import { OfferController } from '@/controllers/offerController.js';
+import { useEffect, useState } from 'react';
+
+const SearchOffers = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [offers, setOffers] = useState([]);
+    const [filteredOffers, setFilteredOffers] = useState([]);
+
+    useEffect(() => {
+        const fetchOffers = async () => {
+            const inputModel = {
+                filter: [],
+                rows: 30,
+                page: 0
+            };
+            const response = await OfferController.list(inputModel);
+            setOffers(response.data || []);
+        };
+
+        fetchOffers();
+    }, []);
+
+    const handleInputChange = (event) => {
+        const query = event.target.value;
+        setSearchQuery(query);
+        const filtered = offers.filter(offer => offer.name.toLowerCase().includes(query.toLowerCase()));
+        setFilteredOffers(filtered);
+    };
+
+    const handleLinkRedirect = (e, link) => {
+        e.preventDefault();
+        window.location.href = `${link}`;
+    }
+    return (
+        <div>
+            <form>
+                <input
+                    type="text"
+                    placeholder="Search offers..."
+                    value={searchQuery}
+                    onChange={handleInputChange}
+                />
+            </form>
+            {filteredOffers.length > 0 && (
+                <div className="popup-search-results">
+                    {filteredOffers.map((offer, index) => (
+                        <div key={index} className="search-result" onClick={(e) => handleLinkRedirect(e, "/offer/" + offer.id)}>
+                            <p>{offer.name} - {offer.disc_price}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default SearchOffers;
---
+++ frontend/src/controllers/groupControllers.js
@@ -0,0 +1,78 @@
+const { baseApiUri } = require("./constants");
+
+class GroupController {
+  static list = async (inputModel) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify(inputModel);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    var response = {};
+    await fetch(baseApiUri + "/group/list", requestOptions)
+      .then((response) => response.text())
+      .then((result) => (response = JSON.parse(result)))
+      .catch((error) => {
+        response = error;
+        console.error(error);
+      });
+    return response;
+  };
+  static add = async (inputModel) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify(inputModel);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    var response = {};
+    await fetch(baseApiUri + "/group/add", requestOptions)
+      .then((response) => response.text())
+      .then((result) => (response = JSON.parse(result)))
+      .catch((error) => {
+        response = error;
+        console.error(error);
+      });
+    return response;
+  };
+
+  static update = async (inputModel) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify(inputModel);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    var response = {};
+    await fetch(baseApiUri + "/group/update", requestOptions)
+      .then((response) => response.text())
+      .then((result) => (response = JSON.parse(result)))
+      .catch((error) => {
+        response = error;
+        console.error(error);
+      });
+    return response;
+  };
+}
+
+module.exports = {
+  GroupController,
+};
