b79db277508d457cb53428fe4cc0312c5a1ea00f
Mateusz <omatix2003@interia.pl>
2024-03-11 15:47:10 +0100

    delete file controller

--- dashboard/src/controllers/fileController.js
+++ dashboard/src/controllers/fileController.js
@@ -93,6 +93,42 @@ class FileController {
 
     return response;
   };
+
+  /**
+   * 
+   * @param {object} inputModel - {
+        "id": int
+    }
+   * @returns 
+   */
+  static delete = async (inputModel) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify(inputModel);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    var response = {};
+    var hash = calculateSHA256(raw);
+    await fetch(baseApiUri + "/file/delete", requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        localStorage.setItem(hash, result);
+        response = JSON.parse(result);
+      })
+      .catch((error) => {
+        response = error;
+        console.error(error);
+      });
+
+    return response;
+  };
 }
 
 module.exports = {
